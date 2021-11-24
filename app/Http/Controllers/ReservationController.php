f43ad52c446b3d0e88ad89f524e7e2e299d0249e
Luis Cordova <cordova0705@gmail.com>
2021-11-23 23:52:55 -0600

    mod

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -9,7 +9,7 @@ class ReservationController extends Controller
 {
     public function info_room($code){
         $reservation = Reservation::where('codigo', $code)->get();
-        return ($reservation->isEmpty()) ? "{}" : Reservation::where('codigo', $code)->get()[0];
+        return ($reservation->isEmpty()) ? "incorrecto" : Reservation::where('codigo', $code)->get()[0];
         #return response()->json(Reservation::where('codigo', $code)->get()[0]);
         #return Reservation::all();
     }
