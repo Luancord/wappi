29c7bf2fd1711505fee88861231a8d5660cb0251
Luis Cordova <cordova0705@gmail.com>
2021-12-10 19:41:51 -0600

    causalidad

---
+++ resources/views/causalidad.blade.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head> 
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- Primary Meta Tags -->
+<title>regalAR</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="title" content="RegalAR">
+<meta name="author" content="RegalAR">
+
+<!--  Social tags -->
+<meta name="keywords" content="RegalAR, realidad aumentada, augmented reallity, AR, RA, bodas, novios, amor">
+<meta name="description" content="">
+
+<!-- Schema.org markup for Google+ -->
+<meta itemprop="name" content="RegalAR">
+<meta itemprop="description" content="Start developing neumorphic web applications and pages using Neumorphism UI. It features over 100 individual components and 5 example pages.">
+
+
+<!-- Favicon -->
+<link rel="apple-touch-icon" sizes="120x120" href="./assets/img/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="./assets/img/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="./assets/img/favicon/favicon-16x16.png">
+<link rel="manifest" href="./assets/img/favicon/site.webmanifest">
+<link rel="mask-icon" href="./assets/img/favicon/safari-pinned-tab.svg" color="#ffffff">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="theme-color" content="#ffffff">
+
+<!-- Fontawesome -->
+<link type="text/css" href="./vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+<!-- Pixel CSS -->
+<link type="text/css" href="./css/neumorphism.css" rel="stylesheet">
+
+<!-- NOTICE: You can use the _analytics.html partial to include production code specific code & trackers -->
+
+</head>
+
+<body>
+    <header class="header-global">
+    <nav id="navbar-main" aria-label="Primary navigation" class="navbar navbar-main navbar-expand-lg navbar-theme-primary headroom navbar-light navbar-transparent navbar-theme-primary">
+        <div class="container position-relative">
+            <a class="navbar-brand shadow-soft py-3 px-3 rounded border border-light mr-lg-4" href="/">
+                <img class="navbar-brand-dark" src="./assets/img/brand/logo.png" alt="Logo light">
+                <img class="navbar-brand-light" src="./assets/img/brand/logo.png" alt="Logo dark">
+            </a>
+            <div class="navbar-collapse collapse" id="navbar_global">
+                <div class="navbar-collapse-header">
+                    <div class="row">
+                        <div class="col-6 collapse-brand">
+                            <a href="./index.html" class="navbar-brand shadow-soft py-2 px-3 rounded border border-light">
+                                <img src="./assets/img/brand/dark.svg" alt="Themesberg logo">
+                            </a>
+                        </div>
+                        <div class="col-6 collapse-close">
+                            <a href="#navbar_global" class="fas fa-times" data-toggle="collapse" data-target="#navbar_global" aria-controls="navbar_global" aria-expanded="false" title="close" aria-label="Toggle navigation"></a>
+                        </div>
+                    </div>
+                </div>
+        </div>
+    </nav>
+</header>
+    <main>
+
+        <!-- Hero -->
+        <section class="section section bg-soft pb-5 overflow-hidden z-2">
+            <div class="container z-2">
+                <div class="row justify-content-center text-center pt-5">
+                    <div class="col-lg-8 col-xl-8">
+                        <h1 class="display-2 mb-3">Causalidad</h1>
+                        <p class="lead px-md-6 mb-5">Invitación digital para la boda entre <strong>novio 1</strong> y <strong>novio 2</strong>.</p>
+                        <div class="d-flex flex-column flex-wrap flex-md-row justify-content-md-center mb-5">
+                        <a class="btn btn-lg btn-primary mr-md-3 mb-3 mb-sm-0" href="itms-services://?action=download-manifest&url=https://360stmx.com/causalidad/ipa/manifest.plist">
+                            <div class="d-flex align-items-center">
+                                <span class="icon icon-lg mr-3"><span class="fab fa-apple"></span></span>
+                                <div class="d-block text-left">
+                                    <small class="font-small">Descarga para</small>
+                                    <div class="h5 mb-0">iPhone</div>
+                                </div>
+                            </div>
+                        </a>
+                        <a class="btn btn-lg btn-primary" href="https://play.google.com/store/apps/details?id=com.RegalAR.Causalidad">
+                            <div class="d-flex align-items-center">
+                                <span class="icon icon-lg mr-3"><span class="fab fa-google-play"></span></span>
+                                <div class="d-block text-left">
+                                    <small class="font-small">Descarga en</small>
+                                    <div class="h5 mb-0">Google Play</div>
+                                </div>
+                            </div>
+                        </a>
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="section section-lg pt-0">
+            <div class="container">
+                <!-- Title -->
+                <div class="row">
+                    <div class="col text-center">
+                        <h2 class="h5 mb-7">Novios</h2>
+                    </div>
+                </div>
+                <!-- End of title-->
+                <div class="row ">
+                    <div class="col-12 col-md-6 offset-lg-2 col-lg-4 mb-5 mb-md-0">
+                    <!-- Profile Card -->
+                        <div class="card bg-primary shadow-soft border-light">
+                            <div class="card-header p-4">
+                                <img src="./assets/img/team/profile-picture-4.jpg" class="card-img-top rounded" alt="Wood Portrait">
+                            </div>
+                            <div class="card-body pt-2">
+                                <h3 class="h5 mb-2">Christopher Wood</h3>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                
+                            </div>
+                        </div>
+                    <!-- End of Profile Card -->
+                    </div>
+                    <div class="col-12 col-md-6 col-lg-4 mb-5 mb-md-0">
+                    <!-- Profile Card -->
+                        <div class="card bg-primary shadow-soft border-light">
+                            <div class="card-header p-4">
+                                <img src="./assets/img/team/profile-picture-4.jpg" class="card-img-top rounded" alt="Wood Portrait">
+                            </div>
+                            <div class="card-body pt-2">
+                                <h3 class="h5 mb-2">Christopher Wood</h3>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                
+                            </div>
+                        </div>
+                    <!-- End of Profile Card -->
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+<!-- End of Section -->
+    </main>
+
+    <!-- Core -->
+<script src="./vendor/jquery/dist/jquery.min.js"></script>
+<script src="./vendor/popper.js/dist/umd/popper.min.js"></script>
+<script src="./vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="./vendor/headroom.js/dist/headroom.min.js"></script>
+
+<!-- Vendor JS -->
+<script src="./vendor/onscreen/dist/on-screen.umd.min.js"></script>
+<script src="./vendor/nouislider/distribute/nouislider.min.js"></script>
+<script src="./vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<script src="./vendor/waypoints/lib/jquery.waypoints.min.js"></script>
+<script src="./vendor/jarallax/dist/jarallax.min.js"></script>
+<script src="./vendor/jquery.counterup/jquery.counterup.min.js"></script>
+<script src="./vendor/jquery-countdown/dist/jquery.countdown.min.js"></script>
+<script src="./vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+<script src="./vendor/prismjs/prism.js"></script>
+
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+
+<!-- Neumorphism JS -->
+<script src="./assets/js/neumorphism.js"></script>
+</body>
+
+</html>
