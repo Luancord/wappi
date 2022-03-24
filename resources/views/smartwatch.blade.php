0e3a1ac54bb4cb28c742fe9ba811acdab3746142
Luis Córdova <cordova0705@gmail.com>
2022-03-24 10:22:24 -0600

    cambios perra

---
+++ resources/views/smartwatch.blade.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head> 
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!-- Primary Meta Tags -->
+<title>regalAR</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="title" content="RegalAR">
+<meta name="author" content="RegalAR">
+
+<!--  Social tags -->
+<meta name="keywords" content="RegalAR, realidad aumentada, augmented reallity, AR, RA, bodas, novios, amor">
+<meta name="description" content="">
+
+<!-- Schema.org markup for Google+ -->
+<meta itemprop="name" content="RegalAR">
+<meta itemprop="description" content="Start developing neumorphic web applications and pages using Neumorphism UI. It features over 100 individual components and 5 example pages.">
+
+
+<!-- Favicon -->
+<link rel="apple-touch-icon" sizes="120x120" href="./assets/img/favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="./assets/img/favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="./assets/img/favicon/favicon-16x16.png">
+<link rel="manifest" href="./assets/img/favicon/site.webmanifest">
+<link rel="mask-icon" href="./assets/img/favicon/safari-pinned-tab.svg" color="#ffffff">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="theme-color" content="#ffffff">
+
+<!-- Fontawesome -->
+<link type="text/css" href="./vendor/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+<!-- Pixel CSS -->
+<link type="text/css" href="./css/neumorphism.css" rel="stylesheet">
+
+<style>
+    .navbar-brand
+{
+    position: relative;
+    background: url("assets/img/brand/logo.png");
+    width: 78px;
+    height:70px;
+    left: 20px;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+</style>
+
+</head>
+
+<body>
+    <header class="header-global">
+    
+</header>
+    <main>
+
+        <!-- Hero -->
+        <section class="section section bg-soft pb-5 overflow-hidden z-2">
+            <div class="container z-2">
+                <div class="row justify-content-center text-center pt-5">
+                    <div class="col-lg-8 col-xl-8">
+                        <h1 class="display-2 mb-3">Smartwatch</h1>
+                    </div>
+                </div>
+                
+                <div class="row justify-content-center text-center pt-5">
+                    <div class="col-lg-8 col-xl-8">
+                        
+                        <a class="btn btn-lg btn-primary mr-md-3 mb-3 mb-sm-0" href="itms-services://?action=download-manifest&url=https://regalar.software/ipa/manifest.plist">
+                            <div class="d-flex align-items-center">
+                                <span class="icon icon-lg mr-3"><span class="fab fa-apple"></span></span>
+                                <div class="d-block text-left">
+                                    <small class="font-small">Descarga para</small>
+                                    <div class="h5 mb-0">Apple Watch</div>
+                                </div>
+                            </div>
+                        </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        
+
+
+
+<!-- End of Section -->
+    </main>
+
+    <!-- Core -->
+<script src="./vendor/jquery/dist/jquery.min.js"></script>
+<script src="./vendor/popper.js/dist/umd/popper.min.js"></script>
+<script src="./vendor/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="./vendor/headroom.js/dist/headroom.min.js"></script>
+
+<!-- Vendor JS -->
+<script src="./vendor/onscreen/dist/on-screen.umd.min.js"></script>
+<script src="./vendor/nouislider/distribute/nouislider.min.js"></script>
+<script src="./vendor/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<script src="./vendor/waypoints/lib/jquery.waypoints.min.js"></script>
+<script src="./vendor/jarallax/dist/jarallax.min.js"></script>
+<script src="./vendor/jquery.counterup/jquery.counterup.min.js"></script>
+<script src="./vendor/jquery-countdown/dist/jquery.countdown.min.js"></script>
+<script src="./vendor/smooth-scroll/dist/smooth-scroll.polyfills.min.js"></script>
+<script src="./vendor/prismjs/prism.js"></script>
+
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+
+<!-- Neumorphism JS -->
+<script src="./assets/js/neumorphism.js"></script>
+</body>
+
+</html>
+itms-services://?action=download-manifest&url=https://regalar.software/ipa/manifest.plistregal123AR"
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('index');
+    return view('smartwatch');
 });
 /*
 Route::get('causalidad', function () {
